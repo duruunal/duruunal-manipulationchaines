c109ac68a4494da867fc5c0d90bf596d6a8a7450
duruunal <duruunal@localhost>
2021-01-07 13:59:40 -0500

    final push

---
+++ tableau/src/Manipulationdeschainesdecaracteres/J4.java
@@ -0,0 +1,20 @@
+package Manipulationdeschainesdecaracteres;
+
+public class J4 
+{
+	
+	public static void main(String[] args) {
+		 int = 0;
+		  for (int i = 0; i < N; i++) 
+		  {
+		    if (          ) 
+		   
+		    System.out.println("");	
+		    	
+		   else (      ) 
+		   
+		   System.out.println(" ");
+	
+		}
+
+
